2961437703225982dfd79e8c5ee4da68d35e8045
valours <valentin@fizix.io>
2018-07-13 14:27:39 +0200

    calendar-ics - add data and use it

---
+++ calendar-ics/data/events.js
@@ -0,0 +1,10 @@
+const events = [
+  {
+    start: [2018, 5, 30, 6, 30],
+    duration: { hour: 6, minutes: 30},
+    title: 'badaboum',
+    description: 'au sol',
+  }
+];
+
+module.exports = events;
--- calendar-ics/index.js
+++ calendar-ics/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 
+const data = require('./data/events');
 app.get('/', function (req, res) {
   res.send('Calendar ics')
 })
 
 app.get('/cal', function(req, res) {
-
+  res.send(data)
 });
 
 app.listen(3000, function () {
-  console.log('banzai event')
+  console.log('banzai')
 })
